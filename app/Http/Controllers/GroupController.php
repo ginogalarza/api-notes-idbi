d69d070488ac79a8e82e616c3f36e2276ecb794a
Gino Galarza Cañari <slipkindo@gmail.com>
2022-10-01 10:33:40 -0500

    Validacion para el grupo y grupo de usuarios.

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Group;
+use App\Models\GroupUsers;
+use Illuminate\Support\Facades\Auth;
 
 class GroupController extends Controller
 {
@@ -15,5 +17,39 @@ class GroupController extends Controller
             'success' => true,
             'data'    => $groups,
         ]); 
+    }
+
+    public function show(Group $group) 
+    {
+        $existGroupUser = GroupUsers::where([
+            ['group_id', $group->id],
+            ['user_id', Auth::user()->id]
+        ])->exists();
+
+        if (!$existGroupUser) {
+            return response()->json([
+                'error'    => true,
+                'messasge' => 'No te has unido aun a este grupo',
+            ]);
+        }
+
+        $notes = GroupUsers::join('notes', 'notes.group_users_id', 'group_users.id')
+            ->where([
+                ['group_id', $group->id],
+                ['user_id', Auth::user()->id]
+            ])
+            ->select(
+                'notes.id as id_note', 
+                'notes.title as title_note', 
+                'notes.description as description_note', 
+                'notes.img as img_note'
+            )
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'group'   => $group,
+            'notes'   => $notes,
+        ]);
     }
 }
--- app/Http/Controllers/GroupUsersController.php
+++ app/Http/Controllers/GroupUsersController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\GroupUsers;
+use App\Models\Group;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class GroupUsersController extends Controller
 {
     public function store(Request $request)
     {
+        $groups = Group::pluck('id')->toArray();
+       
+        $rules = [
+            'groupId' => ['required', Rule::in($groups)],
+        ];
+
+        $messages = [
+            'groupId.required' => 'El "Grupo" es obligatorio.',
+            'groupId.in'       => 'Seleccione un "Grupo" valido.',
+        ];
+
+        $request->validate($rules, $messages);
+       
         GroupUsers::create([
-            'group_id' => $request->group_id,
+            'group_id' => $request->groupId,
             'user_id'  => Auth::user()->id,
         ]);
 
